fa0c3e56c955a540f9b5a68a3abdf6cc044bc728
Dorijan Tomić <dorijan11@gmail.com>
2019-02-22 20:36:33 +0100

    Updates

---
+++ precache-manifest.f7a96ea2a81e0629834ba480b1c0369b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abdf617c8a9cfc0f7b30",
+    "url": "/pokeSearch/static/js/runtime~main.abdf617c.js"
+  },
+  {
+    "revision": "6f4a250fd88545a8e6eb",
+    "url": "/pokeSearch/static/js/main.6f4a250f.chunk.js"
+  },
+  {
+    "revision": "a1d5d574dc2072bcd854",
+    "url": "/pokeSearch/static/js/1.a1d5d574.chunk.js"
+  },
+  {
+    "revision": "6f4a250fd88545a8e6eb",
+    "url": "/pokeSearch/static/css/main.b31e480d.chunk.css"
+  },
+  {
+    "revision": "a1d5d574dc2072bcd854",
+    "url": "/pokeSearch/static/css/1.c5d40e3e.chunk.css"
+  },
+  {
+    "revision": "04ec611ce5b168e0b404a5b29821baf6",
+    "url": "/pokeSearch/index.html"
+  }
+];
